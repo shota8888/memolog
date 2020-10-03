1fb89febfd8fe3e26f6671f84ee9252c20dcdfb9
shota8888 <asbscs@icloud.com>
2020-10-03 11:42:12 +0900

    fix

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -2,15 +2,13 @@ export const GA_TRACKING_ID = process.env.GOOGLE_ANALYTICS_ID
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
-  try {
-    if (window.gtag) {
-      window.gtag('config', GA_TRACKING_ID, {
-        page_path: url,
-      })
-    }
-  } catch(error) {
-    console.log("Error from the trackerPageView => ", error);
+  function gtag() { 
+    window.dataLayer && window.dataLayer.push(arguments); 
   }
+  
+  gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+    })
 }
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
